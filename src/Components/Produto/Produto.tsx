4ff3ebe341b23cc6ff11396c10e19661943a405d
dannunees <danilonunesvieira@gmail.com>
2025-03-18 20:23:12 -0300

    inserindo sistema de login com Clerk

--- src/Components/Produto/Produto.tsx
+++ src/Components/Produto/Produto.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import "../../Styles/Produto/Produto.scss";
 import { ProdutosType } from "../../Types/Types";
-import { SignedIn, SignIn } from "@clerk/clerk-react";
+import { SignedIn} from "@clerk/clerk-react";
 
 
 
